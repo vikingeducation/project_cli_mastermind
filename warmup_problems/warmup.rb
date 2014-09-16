442d677b6c40595bf51c730f2cb2813a3abc64f3
Tom McLaughlin <tim5046@gmail.com>
2014-09-16 10:44:20 -0400

    finished warmup

--- warmup_problems/warmup.rb
+++ warmup_problems/warmup.rb
@@ -14,11 +14,14 @@
 def roll(num=1)
   puts "You rolled #{num} dice. The total is: #{num*(1+rand(6))}" 
 end
-loop do
-  puts "Type quit to exit.\nEnter the number of dice to roll: "
-  numDice=gets.chomp
-  break if numDice=='quit'
-  roll(numDice.to_i)
+
+def doQuestionOne
+  loop do
+    puts "Type quit to exit.\nEnter the number of dice to roll: "
+    numDice=gets.chomp
+    break if numDice=='quit'
+    roll(numDice.to_i)
+  end
 end
 
 #=========================================================================
@@ -46,7 +49,13 @@ def roll_dice(num)
     total
 end
 
-dice_outcomes(3,100)
+def doQuestionTwo
+  puts "Enter the number of dice to throw:"
+  dice=gets.chomp
+  puts "Enter the number of times to throw:"
+  times=gets.chomp
+  dice_outcomes(dice.to_i,times.to_i)
+end
 
 
 #=========================================================================
@@ -64,9 +73,13 @@ def fib(num)
         prev,curr = fibs[-2],fibs[-1]
        fibs<<(prev+curr) 
     end
-    fibs    
+    p fibs    
 end
-fib(7)
+def doQuestionThree
+  puts "How many terms of the Fibonacci sequence do you want?"
+  terms=gets.chomp
+  fib(terms.to_i)
+end
 
 
 
@@ -79,5 +92,79 @@ fib(7)
 #> stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15]) 
 #=> [2, 6]
 
+def stock_picker(arr)
+  max=0
+  days=[]
+  i,j=0,1
+  while i<arr.length
+    while j<arr.length
+        if (arr[j]-arr[i]) > max
+           max = arr[j]-arr[i]
+           days[0],days[1]=i,j
+        end
+        j+=1
+    end
+    i+=1
+    j=i+1
+  end
+  p days
+end
+def doQuestionFour
+  puts "Using data:\n[44, 30, 24, 32, 35, 30, 40, 38, 15]"
+  stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])
+end
 
 
+#=========================================================================
+#Warmup #5: Anagram Testing
+#Write a method anagrams which returns an array of anagrams that can be 
+#made with the specified string. Assume the input is only a single word
+#(e.g. "loot" not "William Shakespeare").
+
+#Example:
+#> anagrams("looter")
+# => ["RETOOL", "ROOTLE", "TOOLER"] 
+#> anagrams("zygote")
+#=> []
+
+def anagrams(word)
+  anagrams=[]
+  f=File.open('dict.txt','r')
+  f.each_line do |i|
+    if i.chomp.length == word.length
+      anagrams << i.chomp if word.chars.sort.join == i.chomp.chars.sort.join
+    end
+  end
+  p anagrams
+end
+def doQuestionFive
+  puts "Enter a word and this will show all anagrams:\n"
+  word=gets.chomp
+  anagrams(word.upcase)
+end
+
+#=========================================================================
+
+#Wrapper method to be able to run any program at once
+puts "Welcome to this awesome set of programs!"
+
+  puts "Type quit to exit."
+  puts "Choose the number of the program that you'd like to run:"
+  puts "1)Rolling Dice\n2)Rolling Dice Outcomes\n3)Fibonacci"
+  puts "4)Stock Picker\n5)Anagram Checker"
+
+  choice = gets.chomp
+  case choice
+    when '1'
+        doQuestionOne
+    when '2'
+        doQuestionTwo
+    when '3'
+        doQuestionThree
+    when '4'
+        doQuestionFour
+    when '5'
+        doQuestionFive
+    else
+        puts "Invalid input. What kind of shenanigans are you trying to pull!?"
+  end
