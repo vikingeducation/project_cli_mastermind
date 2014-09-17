4c09e88611f686e1223b33fd13b3bad16fd8bb6f
Boris Shporkin <boris.shporkin@gmail.com>
2014-09-17 18:13:21 -0400

    second warmup prob complete

--- warmup_problems/warmup.rb
+++ warmup_problems/warmup.rb
@@ -1,4 +1,5 @@
-num_dice=1
+num_dice=1 #defaultnumber of dice
+
 def roll_dice(num_dice)
 	sum=0
 	num_dice.times do
@@ -7,6 +8,26 @@ def roll_dice(num_dice)
 		sum+=number
 	end
 	puts "The total is #{sum}."
+	return sum
 end
 
-roll_dice(3)
+def dice_outcomes(num_dice, num_rolls)
+	#create hash of combos (num_dice up to num_dice*6)
+	combo_hash={}
+	(num_dice..num_dice*6).each do |num|
+	#    combo_hash[num]=0
+		combo_hash[num]=0
+	end
+
+	#roll dice and tally
+	num_rolls.times do
+		sum=roll_dice(num_dice)
+		combo_hash[sum]+=1
+	end
+
+	combo_hash.each do |key, value|
+		puts "#{key}: " + "#"*value
+	end
+end
+
+dice_outcomes(3,100)
