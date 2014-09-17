11e42e6ac61183c174ab2ccc397ff63c4e91a39d
Boris Shporkin <boris.shporkin@gmail.com>
2014-09-17 16:11:12 -0400

    First Warmup Problem

--- warmup_problems/warmup.rb
+++ warmup_problems/warmup.rb
@@ -1,1 +1,12 @@
-# Your code here!
+num_dice=1
+def roll_dice(num_dice)
+	sum=0
+	num_dice.times do
+		number=rand(1..6)
+		puts number
+		sum+=number
+	end
+	puts "The total is #{sum}."
+end
+
+roll_dice(3)
