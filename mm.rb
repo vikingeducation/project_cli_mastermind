207edcc985783a946250d3d7d989d146e2b3817d
Joseph Lozano <lozanojoseph@gmail.com>
2015-06-30 11:29:45 -0700

    filling in classes on mm.rb

--- mm.rb
+++ mm.rb
@@ -1,7 +1,7 @@
 =begin
  Board class 
-   render method - to print board
-   track the board pieces
+   render method - to print board  X
+   track the board pieces X
    check victory
    check full
 
@@ -9,7 +9,7 @@
     place pins 
 
   human class < player class
-      input from human
+      input from human X
   computer class < player class
       input from ai
 
@@ -21,6 +21,119 @@
      tells the board to check victory
        unless victory && !full next
          victor message
+
+
+
+         [ X X X X ]
+         [ X X X X ]
+         [ X X X X ]
+         [ X X X X ]
+         [ X X X X ]
+         [ X X X X ]
+         [ X X X X ]
+         [ X X X X ]
+         [ X X X X ]
+         [ X X X X ]
+         [ X X X X ]
+         [ X X X X ]
+
       
 
+
 =end
+
+
+class Board
+
+  def initialize
+
+    # "X" is a blank space
+    @board = [
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"],
+                ["X", "X", "X", "X"]
+             ]
+
+    @pieces = %w[R G B T A M K E]
+
+    
+
+  end
+
+  def render
+
+    puts
+
+    12.times do |i|
+      p @board[i]
+    end
+    puts
+    puts "Pieces:"
+    p @pieces
+    puts
+
+
+
+  end
+
+end
+
+
+
+class Player
+
+  def place_pin
+
+
+
+  end
+
+end
+
+class Human < Player
+
+  def initialize(board)
+
+  end
+
+  def place_pin
+  end
+
+  def get_imput
+
+    input = ""
+
+    loop do
+      puts "Enter a piece."
+      input = gets.chomp.upcase
+      if @board.pieces.include?(input)
+        break
+      end
+    end      
+  end
+
+end
+
+class Computer < Player
+
+end
+
+
+class Mastermind
+
+  board = Board.new
+  @human = Human.new(board)
+
+
+
+end
+
