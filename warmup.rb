b97062834109abac432d8d8ed73fa839b8694009
mikebutts <mgbutts@gmail.com>
2015-06-30 12:37:36 -0400

    starting warmpup

---
+++ warmup.rb
@@ -0,0 +1,22 @@
+def deep_dup(arr)
+  new_arr = []
+
+  arr.each do |item|
+    new_arr << item
+  end
+  new_arr 
+end
+
+=begin
+def depth (a)
+  return 0 unless a.is_a?(Array)
+  result = 1
+  a.each do(sub)
+    if sub.is_a? (Array)
+    dimension = 
+end
+
+> depth(arrA)
+=> 3
+=end
+
