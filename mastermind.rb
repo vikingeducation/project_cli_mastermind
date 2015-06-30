db330612446ce1a5aef149402657d87bef8fcce2
alokp <alokpra@gmail.com>
2015-06-30 16:54:13 -0700

    human as codebreaker complete

--- mastermind.rb
+++ mastermind.rb
@@ -20,55 +20,126 @@
 #6. Class CodeMaker (not using right now)
 =end
 
+require './deep_dup'
+
+#@board.add_peg_set(Pegset.new)
+#pegs = Pegset.new(solution, guess)
+
 class Mastermind
 	def play
 		guess_count = 0  #could be in Board given historical tracking
 		player_1 = Codebreaker.new
 		player_2 = player_1.opponent
-		game = Board.new(player_2)
-		while guess_count < 2
-			current_ans = player_1.get_ans
-			game.render(current_ans)
-			# p current_ans
-			# p game.solution
+		solution = create_solution(player_2)
+		game = Board.new
+		test = Pegset.new
+		while guess_count < 12
+			current_ans = player_1.get_ans(guess_count)
+			result_of_test = test.check_solution(solution, current_ans)
+			break if result_of_test == "You WIN!"
+			game.add_turn(current_ans, result_of_test)
+			game.render
 			guess_count += 1
 		end
+	end
+
+	def create_solution(player_2)
+		solution = []
+		if player_2 == 0
+		else
+			color_options = ["r","b","g","w"]
+			(color_options.length).times do
+				solution << color_options.sample
+			end
+		end
+		return solution
 	end
 end
 
 class Codebreaker
-	attr_reader :opponent
 	def initialize
-		puts "Hi! Welcome to Mastermind. You have 12 guesses to determine my sequence"
-		puts "Who do you want to play against: human (0) or AI (1)?"   #when asking binary q's, use boolean!!
-		@opponent = gets.chomp.to_i
-		puts "Acceptable input is  r (red), b (blue), g (green), w (white) in the format r,b,g,w"
+		puts "Hi! Welcome to Mastermind. You have 12 guesses to determine my sequence."
 	end
 
-	def get_ans
-		puts "Enter guess:"
+	def opponent
+		puts "Who do you want to play against: human (0) or AI (1)?"   #when asking binary q's, use boolean!!
+		opponent = gets.chomp.to_i
+	end
+
+	def get_ans(guess_no)
+		puts "Input - r(red), b(blue), g(green), w(white) in the format r,b,g,w"
+		puts "Enter guess (#{guess_no + 1} / 12):"
 		current_guess = gets.chomp.split(",")
-		return current_guess
 	end
 end
 
 class Board
-	attr_reader :solution
-	def initialize(opponent)   #move this to Mastermind
+	def initialize
 		@game_state = []
-		if opponent == 0
-		else
-			color_options = ["r","b","g","w"]
-			@solution = [color_options.sample, color_options.sample, color_options.sample, color_options.sample]
-		end
-		return @solution
+		@peg_state = []
 	end
-	def render(new_ans)
+
+	def add_turn(new_ans, test_result)
 		@game_state << new_ans
+		@peg_state << test_result
+	end
+
+	def render
+		p @peg_state[0]
+		p @peg_state[1]
 		@game_state.each_with_index do |row, index|
-			print "Guess #{index+1}:    #{row.join("  ")} \n"
-			#puts
+			print "Guess #{index+1}:    #{row.join("  ")}  "
+			puts "Result: " + " \u2713 "*@peg_state[index][0] + " o "*@peg_state[index][1] + " x "*(4-(@peg_state[index][0]+@peg_state[index][1]))
 		end
 		puts "Solution:   o  o  o  o"
+	end
+
+end
+
+class Pegset
+	def check_solution(solution_needing_duplication, guess_needing_duplication)
+		solution = solution_needing_duplication.deep_dup
+		guess = guess_needing_duplication.deep_dup
+
+		if solution == guess
+			puts "You WIN!"
+		else
+			compare(solution, guess)
+		end
+	end
+
+	def compare(solution, guess)
+		#p solution
+		#p guess
+		exactly_correct = exact_matches(solution, guess)
+		color_correct = color_only_matches(solution,guess) - exactly_correct
+		#p exactly_correct
+		#p color_correct
+		return [exactly_correct, color_correct]
+	end
+
+	def exact_matches(solution, guess)
+		count = 0
+		match_color_position = 0
+		while count < solution.length
+			if (solution[count] == guess[count])
+				match_color_position += 1
+			end
+			count += 1
+		end
+		return match_color_position
+	end
+
+	def color_only_matches(solution, guess)
+		i = 0
+		match_only_color = 0
+		while i < solution.length
+			if solution.include?(guess[i])
+				solution[solution.index(guess[i])] = 0
+				match_only_color += 1
+			end
+			i += 1
+		end
+		return match_only_color
 	end
 end
