8345b2ee3bb1b464a632e6070a7a6dcdeae5919b
Garrett <garrettweinert@gmail.com>
2015-06-30 10:39:08 -0700

    Working on mastermind. At input part

--- mastermind.rb
+++ mastermind.rb
@@ -72,9 +72,24 @@ class Board
   end
 
   def calculate_hint(guess)
+    black_pegs, white_pegs = count_pegs(guess)
+    
+    generate_hint(black_pegs, white_pegs)
+  end
+
+  def generate_hint(black, white)
+    hint = []
+
+    black.times {hint << "B"}
+    white.times {hint << "W"}
+
+    hint << " " until hint.length > 3
+  end
+
+  def count_pegs(guess)
     black_pegs = 0
     white_pegs = 0
-    hint = []
+
     guess.each_with_index do |val, index|
       if val == @winning_condition[index]
         black_pegs += 1
@@ -82,15 +97,40 @@ class Board
         white_pegs += 1
       end
     end
-
-    black_pegs.times {hint << "B"}
-
-    white_pegs.times {hint << "W"}
-
-    until hint.length > 3 do
-      hint << " "
-    end
-
-    hint
+    return black_pegs, white_pegs
   end
+
+
+  def check_win?
+    @guesses[-1] == @winning_condition
+  end
+
 end
+
+# Input
+#   - players = []
+#   - Current player
+#   - Takes guesses until valid depending on player
+
+class Input 
+
+  def get_guess
+    guess_input = gets.chomp.upcase
+    valid_guess?(guess_input)
+
+  end
+
+  def valid_guess?(guess)
+    guess.split(" ").any? { |char| !(["B", "G", "R", "O", "Y", "P" ].include?(char)) }
+
+  end
+
+end
+
+
+
+
+
+
+
+
