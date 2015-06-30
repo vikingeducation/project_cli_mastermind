f015616f6c7679a28265897afe358ac83b3ed802
ayva <olga.ayva@gmail.com>
2015-06-30 13:42:16 -0700

    feedback

--- mastermind.rb
+++ mastermind.rb
@@ -5,30 +5,42 @@ PEGS =["white","black"]
 class Game
 
   def initialize
-
     @code = []
-    @board = Board.new
-    #define variables
-    #randomly choosen code
+    @play_board = Board.new
   end
 
   def create_players
-
     @codemaker = Computer.new
     @codebreaker = Human.new
-
   end
     
   def play
-    @code = @codemaker.generate
+    create_players
 
-    #Game loop started (12 times)
+    @code = @codemaker.generate
+    print "#{@code} - Code generated"
     12.times do
-      @board.add_to_board(@codemaker.guess)
-      puts @board
+      answer = @codebreaker.guess
+      
+      check_win(answer)
+
+      @play_board.add_to_board(answer)
+      p @play_board.board
+
+      @play_board.add_to_board(@codemaker.feedback(answer, @code))
+      p @play_board.board
     end
 
   end
+
+  def check_win(input)
+    if @code == input 
+      puts "You won!" 
+      exit
+    end
+  end
+
+end
 
     #render a board
 
@@ -39,11 +51,27 @@ class Game
     #After loop ends player loses
     #show the code
 
-end
+
 
 class Player
 
+  def feedback(answer, code)  
+    #blacks
+    pegs=[]
+    
+    for i in (0..3) do
+      if answer[i]==code[i] 
+        pegs << "black" 
+      elsif code.any? {|color| color==answer[i]}
+        pegs << "white" 
+      else
+        pegs << 0
+      end
+    end
 
+    puts "#{pegs} - pegs"
+    pegs
+  end
   
 
 end
@@ -58,7 +86,7 @@ class Human < Player
   end
 
   def guess
-    puts "Enter 4 colors from #{COLORS} (like this #{green red blue purple})"
+    puts "Enter 4 colors from #{COLORS} (like this green red blue purple)"
     gets.chomp.split(" ")
   end
 
@@ -71,9 +99,9 @@ class Computer < Player
     new_code = []
 
     4.times do 
-      new_code << COLORS.sample
+      new_code.push(COLORS.sample) 
     end
-
+    puts "#{new_code} New code generated"
     new_code
     
   end
@@ -89,16 +117,12 @@ class Board
   attr_reader :board
 
   def initialize
-
     @board=[]
     #[[4 colors, 4 pegs],[]...12]
-
   end
 
   def add_to_board (input)
-
     @board << input
-
   end
 
   def display_board
@@ -106,3 +130,7 @@ class Board
   end
 
 end
+
+g=Game.new
+
+g.play
