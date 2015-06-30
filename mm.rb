ef27d9bc2b9a9f5105427270f8b53ed20b65ef08
mikebutts <mgbutts@gmail.com>
2015-06-30 13:59:16 -0400

    pseudocoded mastermind

---
+++ mm.rb
@@ -0,0 +1,26 @@
+=begin
+ Board class 
+   render method - to print board
+   track the board pieces
+   check victory
+   check full
+
+ Player class
+    place pins 
+
+  human class < player class
+      input from human
+  computer class < player class
+      input from ai
+
+  Maker
+    sets the solution pieces randomly
+  Mastermind class
+     tell the board to render
+     tell the player to place pieces
+     tells the board to check victory
+       unless victory && !full next
+         victor message
+      
+
+=end
