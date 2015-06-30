c1a91755d72f2d3c7855d3d189c60d7233feac46
Alice Fung <alice.o.fung@gmail.com>
2015-06-30 12:26:41 -0400

    add deep_dup method

---
+++ warmup.rb
@@ -0,0 +1,30 @@
+#deep dup
+
+class Array
+
+  def deep_dup
+    self.map do |item|
+      if item.class == Array
+        item.map do |deep_item|
+          deep_item
+        end
+      else
+        item
+      end
+    end
+  end
+
+end
+
+arr = [ [1,2, ['q']], [3,4,['a']]]
+duper = arr.deep_dup
+p duper.object_id
+p arr.object_id
+puts
+print duper
+p duper[0].object_id
+p duper[3].object_id
+puts
+print arr
+p arr[0].object_id
+p arr[3].object_id
