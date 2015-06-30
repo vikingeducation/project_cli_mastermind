c5b2d5da2a71c49d1c20b845f2f922c5084dd74b
facingsouth <xin.d.liu@gmail.com>
2015-06-30 09:33:44 -0700

    add mastermind Pseudocode

---
+++ matermind.rb
@@ -0,0 +1,14 @@
+Set up game initiallly
+  create game board
+  create players
+  create codes
+
+start game loop
+  reader game board
+  ask for and validate player's guess
+  If the game should end
+    Display the proper message
+    stop looping
+  else
+    add to guess_count
+    keep looping
