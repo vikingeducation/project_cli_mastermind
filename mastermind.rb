e5a0a08ad279ae6a3ba4799913ae283dae910cc0
facingsouth <xin.d.liu@gmail.com>
2015-06-30 17:59:44 -0700

    fully working version of the mastermind with AI

--- mastermind.rb
+++ mastermind.rb
@@ -24,46 +24,51 @@ class Mastermind
   def initialize
 		# set up the board
     @board = Board.new
-		# set up codemaker
-    @code_maker = Computer.new(@board)
-		# set up codebreaker
-    @code_breaker = Player.new(@board)
+    # set up guess limit
+    @guess_limit = 12
   end
 
   def welcome
     puts "Welcome to Mastermind!"
     puts "Enter guesses in the form of bbbb "
-    puts "The code consists of any combination of (b)lue, (g)reen, (y)ellow, (o)range, (p)urple, or (r)ed."
+    puts "The code consists of any combination of "
+    puts "(b)lue, (g)reen, (y)ellow, (o)range, (p)urple, or (r)ed."
     puts "You have 12 tries. Good luck!"
   end
 
-	# play
-  def play
+  def play(play_code_breaker = true)
     welcome
-    @code_maker.get_code
-		# loop indefinitely
-    loop do
-      
-			# call board rendering method
-      @board.render
-			# ask for guess from codebreaker
-      @code_breaker.get_guess
 
-			# break the loop IF game is over
-      break if check_game_over
+    # set up codemaker and codebreaker
+    if play_code_breaker
+      @code_maker = Computer.new(@board)
+      @code_breaker = CodeBreaker.new(@board)
+    else
+      @code_maker = CodeMaker.new(@board)
+      @code_breaker = AI.new(@board)
     end
 
-    if @code_breaker.guess_count > 12
-      puts "You lose." 
+    @code_maker.get_code
+		
+    # loop 12 times
+    @guess_limit.times do
+
+			# ask for guess from codebreaker
+      @code_breaker.get_guess
+      # call board rendering method
+      @board.render
+			# break the loop IF game is over
+      break if @board.check_victory?
+    end
+
+
+    if @board.check_victory?
+      puts "The guess is correct!"
+    else
+      puts "You're out of guesses!" 
       print "The code is: "
       @board.show_code
     end
-
-    puts "You win." if @board.check_victory?
-  end
-
-	def check_game_over
-		@code_breaker.guess_count > 12 || @board.check_victory?
   end
 
 end
@@ -83,6 +88,7 @@ class Board
       print feedback
       print "\n"
     end
+    puts "---------------------------------------"
   end
 
   def add_guess(guess)
@@ -126,10 +132,8 @@ end
 
 class CodeBreaker
   PEG_COLORS = %w( b g o r p y)
-  attr_reader :guess_count
 
   def initialize(board)
-    @guess_count = 0
     @board = board
   end
 
@@ -138,7 +142,6 @@ class CodeBreaker
       guess = ask_for_guess
       if is_valid_guess?(guess)
         @board.add_guess(guess)
-        add_guess_count
         break
       end
     end
@@ -162,11 +165,6 @@ class CodeBreaker
       return true
     end
   end
-
-  def add_guess_count
-    @guess_count += 1
-  end
-
 end
 
 class CodeMaker
@@ -219,16 +217,82 @@ end
 
 class AI < CodeBreaker
 
-  def get_guess
-    @board.add_guess(guess)
-    add_guess_count
+  def initialize(board)
+    super(board)
+    @guess = [0, 0, 1, 1]
+    @candidates = generate_possible_solutions
   end
 
-  #create all posible codes
-  #start with initial guess 1122
-  #if guess==code break
+  def get_guess
+    @board.add_guess(convert_num_to_guess(@guess))
+    generate_guess
+  end
+
+  # codes = [all posibile codes]
   #
+  # initial_guess = 0011
+  #
+  # loop
+  #   feedback = get_feedback(initial_guess, real_code)
+
+  #   codes.each do |code|
+  #     if get_feedback(initial_guess, code) == feedback
+  #       remove code from codes
+  #     end
+  #   end
+
+  #   initial_guess = codes[0]
+  # end
+
   def generate_guess
+    feedback = @board.generate_feedback_key(convert_num_to_guess(@guess))
+
+    @candidates = @candidates.select do |c|
+      get_feedback(convert_num_to_guess(@guess), convert_num_to_guess(c)) == feedback
+    end
+
+    @guess = @candidates[0]
+  end
+
+  def generate_possible_solutions
+    candidates = []
+    vals = [0, 1, 2, 3, 4, 5]
+    vals.each do |v1|
+      vals.each do |v2|
+        vals.each do |v3|
+          vals.each do |v4|
+            candidates << [v1, v2, v3, v4]
+          end
+        end
+      end
+    end
+    candidates
+  end
+
+  def convert_num_to_guess(nums)
+    nums.map {|i| PEG_COLORS[i]}
+  end
+
+  def get_feedback(guess, code)
+    feedback_key = []
+    code_dup = code.dup
+    index_black_list = []
+    guess.each_with_index do |p, i|
+
+      if p == code_dup[i]
+        feedback_key << "b"
+        code_dup[i] = "q"
+        index_black_list << i
+      end
+    end
+
+    guess.each_with_index do |p, i|
+      if code_dup.include?(p) && !index_black_list.include?(i)
+        feedback_key << "w"
+      end
+    end
+
+    feedback_key.sort
   end
 end 
 
