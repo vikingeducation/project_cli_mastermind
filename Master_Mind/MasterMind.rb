58c92836be8250f8daefbee121e6e825e71e013c
Steven Chang <prime_pork@hotmail.com>
2015-12-02 23:34:38 +1000

    game now takes players moves and renders board accordingly and also gives player option to submit once their guess is full

--- Master_Mind/MasterMind.rb
+++ Master_Mind/MasterMind.rb
@@ -1,11 +1,15 @@
 class MasterMind
+
+  attr_reader :round
+
   def initialize
-    @board = Board.new
+    @board = Board.new(self, @code_breaker)
     @computer = Computer.new
     @mode = nil
     @master_code = nil
-    @code_breaker = Player.new
-    @code_maker = Player.new
+    @code_breaker = Player.new(self, @board)
+    @code_maker = Player.new(self, @board)
+    @round = 1
   end
 
   def ask_for_mode
@@ -53,46 +57,60 @@ class MasterMind
     puts ""
     ask_for_mode
     ask_for_master_code
-    start_round
+    play_game
   end
 
-  def start_round
-    until @code_breaker.response == @master_code
-      puts "--------------------------"
-      puts ""
-      puts "Legend"
-      puts "$ = exact"
-      puts "% = right but in the wrong position"
-      puts ""
-      puts "   -Q-W-E-R-"
-      puts "12 -X-X-X-X-"
-      puts "11 -X-X-X-X-"
-      puts "10 -X-X-X-X-"
-      puts "9  -X-X-X-X-"
-      puts "8  -X-X-X-X-"
-      puts "7  -X-X-X-X-"
-      puts "6  -X-X-X-X-"
-      puts "5  -X-X-X-X-"
-      puts "4  -X-X-X-X-"
-      puts "3  -X-X-X-X-"
-      puts "2  -X-X-X-X-"
-      puts "1  -X-X-X-X-"
-      puts "   -Q-W-E-R-"
-      puts ""
-      puts "1-2-3-4-5-6-7-8-PEGS"
-      puts "A-S-D-F-G-H-J-K-PEG HOLDER"
-      puts ""
-      print "Which peg do you want to move (1 to 8): "
-      chosen_peg = @code_breaker.choose_peg
-      print "Where do you want the peg to go (Q,W,E,R,A,S etc): "
-      peg_destination = @code_breaker.choose_destination
+  def play_game
+    @round = 1
+    until @code_breaker.response == @master_code || round == 13
+      @board.pegs = %w[1 2 3 4 5 6 7 8]
+      @code_breaker.play_round
       @code_breaker.response = @master_code
+      @round += 1
     end
   end
 end
 
 class Board
 
+  attr_accessor :board, :pegs, :peg_holder, :board_position
+
+  def initialize(game, player)
+    @mastermind = game
+    @code_breaker = player
+    @board = []
+    12.times { @board << %w[X X X X] }
+    @pegs = %w[1 2 3 4 5 6 7 8]
+    @peg_holder = %w[a s d f g h j k]
+    @board_position = %w[q w e r]
+  end
+
+  def render
+    puts "--------------------------"
+    puts ""
+    puts "Legend"
+    puts "$ = exact"
+    puts "% = right but in the wrong position"
+    puts ""
+    puts "   -Q-W-E-R-"
+    puts "12 -#{@board[11][0]}-#{@board[11][1]}-#{@board[11][2]}-#{@board[11][3]}-"
+    puts "11 -#{@board[10][0]}-#{@board[10][1]}-#{@board[10][2]}-#{@board[10][3]}-"
+    puts "10 -#{@board[9][0]}-#{@board[9][1]}-#{@board[9][2]}-#{@board[9][3]}-"
+    puts "9  -#{@board[8][0]}-#{@board[8][1]}-#{@board[8][2]}-#{@board[8][3]}-"
+    puts "8  -#{@board[7][0]}-#{@board[7][1]}-#{@board[7][2]}-#{@board[7][3]}-"
+    puts "7  -#{@board[6][0]}-#{@board[6][1]}-#{@board[6][2]}-#{@board[6][3]}-"
+    puts "6  -#{@board[5][0]}-#{@board[5][1]}-#{@board[5][2]}-#{@board[5][3]}-"
+    puts "5  -#{@board[4][0]}-#{@board[4][1]}-#{@board[4][2]}-#{@board[4][3]}-"
+    puts "4  -#{@board[3][0]}-#{@board[3][1]}-#{@board[3][2]}-#{@board[3][3]}-"
+    puts "3  -#{@board[2][0]}-#{@board[2][1]}-#{@board[2][2]}-#{@board[2][3]}-"
+    puts "2  -#{@board[1][0]}-#{@board[1][1]}-#{@board[1][2]}-#{@board[1][3]}-"
+    puts "1  -#{@board[0][0]}-#{@board[0][1]}-#{@board[0][2]}-#{@board[0][3]}-"
+    puts "   -Q-W-E-R-"
+    puts ""
+    puts "#{@pegs[0]}-#{@pegs[1]}-#{@pegs[2]}-#{@pegs[3]}-#{@pegs[4]}-#{@pegs[5]}-#{@pegs[6]}-#{@pegs[7]}-PEGS"
+    puts "A-S-D-F-G-H-J-K-PEG HOLDER"
+    puts ""
+  end
 end
 
 class Computer
@@ -108,8 +126,10 @@ class Player
 
   attr_accessor :response
 
-  def initialize
+  def initialize(game, board)
+    @mastermind = game
     @response = nil
+    @board = board
   end
 
   def choose_destination
@@ -149,6 +169,17 @@ class Player
     false
   end
 
+  def submit_guess?(board_for_the_round)
+    if !((board_for_the_round).include? 'X')
+      print "Type 's' then enter to submit your answer: "
+      respond
+      if @response.downcase == 's'
+        return true
+      end
+    end
+    false
+  end
+
   def make_a_code
     respond
     until code_is_valid?
@@ -170,6 +201,26 @@ class Player
   def peg_is_valid?
     return true if (1..8).include? @response.to_i
     false
+  end
+
+  def play_round
+    @board.render
+    until submit_guess?(@board.board[@mastermind.round-1])
+      print "Which peg do you want to move (1 to 8): "
+      chosen_peg = choose_peg
+      print "Where do you want the peg to go (Q,W,E,R,A,S etc): "
+      peg_destination = choose_destination
+      if (@board.pegs.include? chosen_peg) && (@board.peg_holder.include? peg_destination.downcase)
+        @board.pegs[@board.pegs.index(chosen_peg)], @board.pegs[@board.peg_holder.index(peg_destination.downcase)] = @board.pegs[@board.peg_holder.index(peg_destination.downcase)], @board.pegs[@board.pegs.index(chosen_peg)]
+      elsif (@board.pegs.include? chosen_peg) && (@board.board_position.include? peg_destination.downcase)
+        @board.pegs[@board.pegs.index(chosen_peg)], @board.board[@mastermind.round-1][@board.board_position.index(peg_destination.downcase)] = @board.board[@mastermind.round-1][@board.board_position.index(peg_destination.downcase)], @board.pegs[@board.pegs.index(chosen_peg)]
+      elsif (@board.board[@mastermind.round-1].include? chosen_peg) && (@board.peg_holder.include? peg_destination.downcase)
+        @board.board[@mastermind.round-1][@board.board[@mastermind.round-1].index(chosen_peg)], @board.pegs[@board.peg_holder.index(peg_destination.downcase)] = @board.pegs[@board.peg_holder.index(peg_destination.downcase)], @board.board[@mastermind.round-1][@board.board[@mastermind.round-1].index(chosen_peg)]
+      else
+        @board.board[@mastermind.round-1][@board.board[@mastermind.round-1].index(chosen_peg)], @board.board[@mastermind.round-1][@board.board_position.index(peg_destination.downcase)] = @board.board[@mastermind.round-1][@board.board_position.index(peg_destination.downcase)], @board.board[@mastermind.round-1][@board.board[@mastermind.round-1].index(chosen_peg)]
+      end
+    @board.render
+    end
   end
 
   def quit?
