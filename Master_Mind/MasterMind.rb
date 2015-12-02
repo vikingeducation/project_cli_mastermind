843cd7ddce9b2c1c0f5d426287eb6582bcaf30a2
Steven Chang <prime_pork@hotmail.com>
2015-12-02 15:45:06 +1000

    asked the player to choose a peg to move and then destination for that peg and it works.

--- Master_Mind/MasterMind.rb
+++ Master_Mind/MasterMind.rb
@@ -1,17 +1,18 @@
 class MasterMind
   def initialize
-    @player1 = Player.new
-    @player2 = Player.new
+    @board = Board.new
     @computer = Computer.new
     @mode = nil
     @master_code = nil
+    @code_breaker = Player.new
+    @code_maker = Player.new
   end
 
   def ask_for_mode
     puts "Type 'quit' and then enter, at any point, to exit the game."
     puts ""
-    puts "Press '1' or '2' and then enter, to set the mode: "
-    @mode = @player1.set_mode
+    print "Press '1' or '2' and then enter, to set the mode: "
+    @mode = @code_breaker.set_mode
     puts ""
   end
 
@@ -21,7 +22,7 @@ class MasterMind
     else
       puts "Code Maker, set a code of four numbers from '1' to '8'."
       print "Each number must be different (e.g. 1234): "
-      @master_code = @player2.make_a_code
+      @master_code = @code_maker.make_a_code
     end
   end
 
@@ -52,13 +53,81 @@ class MasterMind
     puts ""
     ask_for_mode
     ask_for_master_code
+    start_round
+  end
+
+  def start_round
+    until @code_breaker.response == @master_code
+      puts "--------------------------"
+      puts ""
+      puts "Legend"
+      puts "$ = exact"
+      puts "% = right but in the wrong position"
+      puts ""
+      puts "   -Q-W-E-R-"
+      puts "12 -X-X-X-X-"
+      puts "11 -X-X-X-X-"
+      puts "10 -X-X-X-X-"
+      puts "9  -X-X-X-X-"
+      puts "8  -X-X-X-X-"
+      puts "7  -X-X-X-X-"
+      puts "6  -X-X-X-X-"
+      puts "5  -X-X-X-X-"
+      puts "4  -X-X-X-X-"
+      puts "3  -X-X-X-X-"
+      puts "2  -X-X-X-X-"
+      puts "1  -X-X-X-X-"
+      puts "   -Q-W-E-R-"
+      puts ""
+      puts "1-2-3-4-5-6-7-8-PEGS"
+      puts "A-S-D-F-G-H-J-K-PEG HOLDER"
+      puts ""
+      print "Which peg do you want to move (1 to 8): "
+      chosen_peg = @code_breaker.choose_peg
+      print "Where do you want the peg to go (Q,W,E,R,A,S etc): "
+      peg_destination = @code_breaker.choose_destination
+      @code_breaker.response = @master_code
+    end
+  end
+end
+
+class Board
+
+end
+
+class Computer
+  def make_a_code
+    numbers = [1,2,3,4,5,6,7,8]
+    master_code = []
+    4.times {master_code << numbers.shuffle!.pop}
+    master_code.join('')
   end
 end
 
 class Player
 
+  attr_accessor :response
+
   def initialize
     @response = nil
+  end
+
+  def choose_destination
+    respond
+    until destination_is_valid?
+      print "Invalid selection! Enter a position on the board (Q, W, E or R) or a position on the peg holder (A, S, D, F, G, H, J, K): "
+      respond
+    end
+    @response
+  end
+
+  def choose_peg
+    respond
+    until peg_is_valid?
+      print "Invalid selection! Enter the number of the peg you want to move (1 to 8): "
+      respond
+    end
+    @response
   end
 
   def code_is_valid?
@@ -73,6 +142,11 @@ class Player
       return false
     end
     true
+  end
+
+  def destination_is_valid?
+    return true if ['q','w','e','r','a','s','d','f','g','h','j','k','l'].include? @response.downcase
+    false
   end
 
   def make_a_code
@@ -90,6 +164,11 @@ class Player
     if @response == '1' || @response == '2'
       return true
     end
+    false
+  end
+
+  def peg_is_valid?
+    return true if (1..8).include? @response.to_i
     false
   end
 
@@ -112,15 +191,6 @@ class Player
     @response
   end
 
-end
-
-class Computer
-  def make_a_code
-    numbers = [1,2,3,4,5,6,7,8]
-    master_code = []
-    4.times {master_code << numbers.shuffle!.pop}
-    master_code.join('')
-  end
 end
 
 MasterMind.new.start_game
