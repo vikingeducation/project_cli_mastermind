62d2097969a580043792b55987cd6608f33cd41a
Steven Chang <prime_pork@hotmail.com>
2015-12-07 12:00:55 +1000

    refactored code more and also made parts of all classes private

--- Master_Mind/MasterMind.rb
+++ Master_Mind/MasterMind.rb
@@ -1,32 +1,15 @@
 class MasterMind
 
-  attr_accessor :round, :master_code
+  attr_accessor :round, :master_code, :mode, :computer
+  attr_reader :human
 
   def initialize
-    @board = Board.new(self, @human)
+    @board = Board.new(self, human)
     @computer = Computer.new(self, @board)
     @mode = nil
     @master_code = nil
     @human = Human.new(self, @board)
     @round = 1
-  end
-
-  def ask_for_mode
-    puts "Type 'quit' and then enter, at any point, to exit the game."
-    puts ""
-    print "Press '1' (to play as the Code Breaker) or '2' (as the Code Maker) and then enter, to set the mode: "
-    @mode = @human.set_mode
-    puts ""
-  end
-
-  def ask_for_master_code
-    if @mode == '1'
-      @master_code = @computer.make_a_code
-    else
-      puts "Code Maker, set a code of four pegs from these pegs (!, @, #, $, %, ^, & , *)."
-      print "Each peg must be different (e.g. !@#$): "
-      @master_code = @human.make_a_code
-    end
   end
 
   def start_game
@@ -59,24 +42,43 @@ class MasterMind
     play_game
   end
 
+  private
+
+  def ask_for_mode
+    puts "Type 'quit' and then enter, at any point, to exit the game."
+    puts ""
+    print "Press '1' (to play as the Code Breaker) or '2' (as the Code Maker) and then enter, to set the mode: "
+    @mode = human.set_mode
+    puts ""
+  end
+
+  def ask_for_master_code
+    if mode == '1'
+      @master_code = @computer.make_a_code
+    else
+      puts "Code Maker, set a code of four pegs from these pegs (!, @, #, $, %, ^, & , *)."
+      print "Each peg must be different (e.g. !@#$): "
+      @master_code = human.make_a_code
+    end
+  end
+
+  def delegate_player
+    if @mode == '1'
+      human.play_round
+    else
+      @computer.play_round
+    end
+  end
+
   def play_game
-    @round = 1
     @board.render
     until @board.clue_board[@round - 2] == ['E','E','E','E'] || @round == 13
-      if @mode == '1'
-        @human.play_round
-      else
-        @computer.play_round(@round)
-      end
+      delegate_player
       @board.pegs = %w[! @ # $ % ^ & *]
       @board.render
       @round += 1
     end
     display_outcome
-    print @computer.definite
-    print @computer.definitely_not
-    print @computer.exact_positions
-    print @computer.maybe
   end
 
   def display_outcome
@@ -111,18 +113,72 @@ class Board
   end
 
   def assess_guess
-    @clue_board[@mastermind.round-1] = []
-    @board[@mastermind.round-1].each_with_index do |peg, index|
-      if peg == @mastermind.master_code[index]
-        @clue_board[@mastermind.round-1] << 'E'
-      elsif @mastermind.master_code.include? peg
-        @clue_board[@mastermind.round-1] << "R"
+    @clue_board[round] = []
+    return_clues
+    organise_clues
+  end
+
+  def render
+    puts "--------------------------"
+    puts ""
+    puts "Legend"
+    puts "E = exact"
+    puts "R = right but in the wrong position"
+    puts ""
+    puts "   -1-2-3-4- KEYPAD"
+
+    i = 11
+    while i >= 0
+      if i + 1 >= 10
+        puts "#{i + 1} -#{@board[i][0]}-#{@board[i][1]}-#{@board[i][2]}-#{@board[i][3]}- #{@clue_board[i].join('')}"
+      else
+        puts "#{i + 1}  -#{@board[i][0]}-#{@board[i][1]}-#{@board[i][2]}-#{@board[i][3]}- #{@clue_board[i].join('')}"
       end
+      i -= 1
     end
-    if @clue_board[@mastermind.round-1].empty?
-      @clue_board[@mastermind.round-1] = %w[F A I L !]
+
+    puts "   -1-2-3-4- KEYPAD"
+    puts ""
+    puts "   -#{@pegs[0]}-#{@pegs[1]}-#{@pegs[2]}-#{@pegs[3]}-"
+    puts "   -Q-W-E-R- KEYPAD"
+    puts ""
+    puts "   -#{@pegs[4]}-#{@pegs[5]}-#{@pegs[6]}-#{@pegs[7]}-"
+    puts "   -A-S-D-F- KEYPAD"
+    puts ""
+  end
+
+  def switch_pegs(chosen_key, destination_key)
+    round_array = round
+    chosen_key_index = find_key_index(chosen_key)
+    destination_key_index = find_key_index(destination_key)
+    if @peg_holder.include? chosen_key
+      switch_when_from_peg_holder(destination_key, chosen_key_index, destination_key_index, round_array)
     else
-      @clue_board[@mastermind.round-1].sort!
+      switch_when_from_board(destination_key, round_array, chosen_key_index, destination_key_index)
+    end
+  end
+
+  private
+
+  def round
+    round = @mastermind.round - 1
+  end
+
+  def organise_clues
+    if @clue_board[round].empty?
+      @clue_board[round] = %w[F A I L !]
+    else
+      @clue_board[round].sort!
+    end
+  end
+
+  def return_clues
+    @board[round].each_with_index do |peg, index|
+      if peg == @mastermind.master_code[index]
+        @clue_board[round] << 'E'
+      elsif @mastermind.master_code.include? peg
+        @clue_board[round] << "R"
+      end
     end
   end
 
@@ -135,51 +191,20 @@ class Board
     key_index
   end
 
-  def switch_pegs(chosen_key, destination_key)
-    round_array = @mastermind.round - 1
-    chosen_key_index = find_key_index(chosen_key)
-    destination_key_index = find_key_index(destination_key)
-    if @peg_holder.include? chosen_key.downcase
-      if @peg_holder.include? destination_key.downcase
+  def switch_when_from_peg_holder(destination_key, chosen_key_index, destination_key_index, round_array)
+    if @peg_holder.include? destination_key
         @pegs[chosen_key_index], @pegs[destination_key_index] = @pegs[destination_key_index], @pegs[chosen_key_index]
-      else
-        @pegs[chosen_key_index], @board[round_array][destination_key_index] = @board[round_array][destination_key_index], @pegs[chosen_key_index]
-      end
-    elsif @peg_holder.include? destination_key.downcase
+    else
+      @pegs[chosen_key_index], @board[round_array][destination_key_index] = @board[round_array][destination_key_index], @pegs[chosen_key_index]
+    end
+  end
+
+  def switch_when_from_board(destination_key, round_array, chosen_key_index, destination_key_index)
+    if @peg_holder.include? destination_key
       @board[round_array][chosen_key_index], @pegs[destination_key_index] = @pegs[destination_key_index], @board[round_array][chosen_key_index]
     else
       @board[round_array][chosen_key_index], @board[round_array][destination_key_index] = @board[round_array][destination_key_index], @board[round_array][chosen_key_index]
     end
-  end
-
-  def render
-    puts "--------------------------"
-    puts ""
-    puts "Legend"
-    puts "E = exact"
-    puts "R = right but in the wrong position"
-    puts ""
-    puts "   -1-2-3-4- KEYPAD"
-    puts "12 -#{@board[11][0]}-#{@board[11][1]}-#{@board[11][2]}-#{@board[11][3]}- #{@clue_board[11].join('')}"
-    puts "11 -#{@board[10][0]}-#{@board[10][1]}-#{@board[10][2]}-#{@board[10][3]}- #{@clue_board[10].join('')}"
-    puts "10 -#{@board[9][0]}-#{@board[9][1]}-#{@board[9][2]}-#{@board[9][3]}- #{@clue_board[9].join('')}"
-    puts "9  -#{@board[8][0]}-#{@board[8][1]}-#{@board[8][2]}-#{@board[8][3]}- #{@clue_board[8].join('')}"
-    puts "8  -#{@board[7][0]}-#{@board[7][1]}-#{@board[7][2]}-#{@board[7][3]}- #{@clue_board[7].join('')}"
-    puts "7  -#{@board[6][0]}-#{@board[6][1]}-#{@board[6][2]}-#{@board[6][3]}- #{@clue_board[6].join('')}"
-    puts "6  -#{@board[5][0]}-#{@board[5][1]}-#{@board[5][2]}-#{@board[5][3]}- #{@clue_board[5].join('')}"
-    puts "5  -#{@board[4][0]}-#{@board[4][1]}-#{@board[4][2]}-#{@board[4][3]}- #{@clue_board[4].join('')}"
-    puts "4  -#{@board[3][0]}-#{@board[3][1]}-#{@board[3][2]}-#{@board[3][3]}- #{@clue_board[3].join('')}"
-    puts "3  -#{@board[2][0]}-#{@board[2][1]}-#{@board[2][2]}-#{@board[2][3]}- #{@clue_board[2].join('')}"
-    puts "2  -#{@board[1][0]}-#{@board[1][1]}-#{@board[1][2]}-#{@board[1][3]}- #{@clue_board[1].join('')}"
-    puts "1  -#{@board[0][0]}-#{@board[0][1]}-#{@board[0][2]}-#{@board[0][3]}- #{@clue_board[0].join('')}"
-    puts "   -1-2-3-4- KEYPAD"
-    puts ""
-    puts "   -#{@pegs[0]}-#{@pegs[1]}-#{@pegs[2]}-#{@pegs[3]}-"
-    puts "   -Q-W-E-R- KEYPAD"
-    puts ""
-    puts "   -#{@pegs[4]}-#{@pegs[5]}-#{@pegs[6]}-#{@pegs[7]}-"
-    puts "   -A-S-D-F- KEYPAD"
-    puts ""
   end
 
 end
@@ -195,7 +220,7 @@ end
 
 class Computer < Player
 
-  attr_accessor :definite, :definitely_not
+  attr_accessor :definite, :definitely_not, :round
   attr_reader :exact_positions, :maybe
 
   def initialize(game, board)
@@ -204,48 +229,9 @@ class Computer < Player
     @definitely_not = []
     @maybe = []
     @exact_positions = {}
-  end
-
-  def assess_results(round)
-    e_count_current = 0
-    e_count_previous = 0
-    @board.clue_board[round-1].each { |e| e_count_current += 1 if e == "E"}
-    @board.clue_board[round-2].each { |e| e_count_previous += 1 if e == "E"}
-    if round >= 2 && round <= 5
-      if @board.clue_board[round-1].size > @board.clue_board[round-2].size
-        @definite << @board.board[round-1][round-2]
-        @definitely_not << @board.pegs[round-2]
-        check_for_exacts(round, e_count_previous, e_count_current)
-      elsif @board.clue_board[round-1].size < @board.clue_board[round-2].size
-        @definite << @board.pegs[round-2]
-        @definitely_not << @board.board[round-1][round-2]
-        check_for_exacts(round, e_count_previous, e_count_current)
-      elsif @board.clue_board[round-1] == @board.clue_board[round-2]
-        # This is going to take some work 
-        @maybe << @board.board[round-1][round-2]
-        @maybe << @board.pegs[round-2]
-      else
-        @definite << @board.board[round-1][round-2]
-        @definite << @board.pegs[round-2]
-        check_for_exacts(round, e_count_previous, e_count_current)
-      end
-    end
-    if round > 5 && @maybe.size == 2
-
-    end
-  end
-
-  # Dealing with maybes
-  # First I guess you'd put all your definites in place.
-  # Then you'd put your definites in.
-  # Then you'd start subbing in one of the maybes at a time to figure out if it's supposed to be there. 
-
-  def check_for_exacts(round, e_count_previous, e_count_current)
-    if e_count_current - e_count_previous == 1
-      @exact_positions[@board.board[round-1][round-2]] = round-2
-    elsif e_count_previous - e_count_current == 1
-      @exact_positions[@board.board[round-2][round-2]] = round-2
-    end
+    @wrong_positions = {}
+    @e_count_current = 0
+    @e_count_previous = 0
   end
 
   def make_a_code
@@ -255,18 +241,76 @@ class Computer < Player
     master_code
   end
 
-  def play_round(round)
+  def play_round
     pegs = %w[! @ # $ % ^ & *]
-    if round == 1
-      @board.board[round-1] = %w[! @ # $]
-    elsif round < 6
-      new_array = @board.board[round-2]
-      @board.board[round-1] = new_array.dup
-      @board.board[round-1][round - 2] = @board.pegs[round + 2]
+    if current_round == 0
+      @board.board[current_round] = %w[! @ # $]
+    elsif current_round < 5
+      new_array = @board.board[previous_round]
+      @board.board[current_round] = new_array.dup
+      @board.board[current_round][previous_round] = @board.pegs[current_round + 3]
     end
     @board.assess_guess
-    assess_results(round)
+    assess_results
   end
+
+  private
+
+  def current_round
+    @mastermind.round - 1
+  end
+
+  def previous_round
+    @mastermind.round - 2
+  end
+
+  def assess_results
+    count_exacts
+    if current_round >= 1 && current_round <= 4
+      delegate_definites_maybes
+      check_for_exacts
+    end
+  end
+
+  def count_exacts
+    @e_count_current = 0
+    @e_count_previous = 0
+    @board.clue_board[current_round].each { |e| @e_count_current += 1 if e == "E"}
+    @board.clue_board[previous_round].each { |e| @e_count_previous += 1 if e == "E"}
+  end
+
+  def delegate_definites_maybes
+    if @board.clue_board[current_round].size > @board.clue_board[previous_round].size
+      @definite << @board.board[current_round][previous_round]
+      @definitely_not << @board.pegs[previous_round]
+    elsif @board.clue_board[current_round].size < @board.clue_board[previous_round].size
+      @definite << @board.pegs[previous_round]
+      @definitely_not << @board.board[current_round][previous_round]
+    else
+      delegate_maybes
+    end
+  end
+
+  def delegate_maybes
+    if @board.clue_board[current_round] == @board.clue_board[previous_round]
+      # This is going to take some work 
+      @maybe << @board.board[current_round][previous_round]
+      @maybe << @board.pegs[previous_round]
+    else
+      @definite << @board.board[current_round][previous_round]
+      @definite << @board.pegs[previous_round]
+    end
+  end
+
+
+  def check_for_exacts
+    if @e_count_current - @e_count_previous == 1
+      @exact_positions[@board.board[current_round][previous_round]] = previous_round
+    elsif @e_count_previous - @e_count_current == 1
+      @exact_positions[@board.board[previous_round][previous_round]] = previous_round
+    end
+  end
+
 end
 
 class Human < Player
@@ -278,14 +322,55 @@ class Human < Player
     @response = nil
   end
 
+  def make_a_code
+    respond
+    until code_is_valid?
+      puts "The code you have entered is invalid!"
+      puts "Code Maker, set a code of four pegs from these pegs (!, @, #, $, %, ^, & , *)."
+      print "Each peg must be different (e.g. !@#$): "
+      respond
+    end
+    response.split('')
+  end
+
+  def play_round
+    loop do
+      print "Move from (KEYPAD position) or 'z' to submit code: "
+      chosen_peg = choose_key
+      break if chosen_peg.downcase == 'z'
+      print "Move to (KEYPAD position) or 'z' to submit code: "
+      peg_destination = choose_key
+      break if peg_destination.downcase == 'z'
+      @board.switch_pegs(chosen_peg, peg_destination)
+      @board.render
+    end
+    @board.assess_guess
+  end
+
+  def set_mode
+    respond
+    until mode_is_valid?
+      puts "The mode you have entered is invalid!"
+      print "Press '1' or '2' and then enter, to set the mode: "
+      respond
+    end
+    @response
+  end
+
+  private
+
+  def response
+    @response.downcase
+  end
+
   def choose_key
     respond
     until key_is_valid?
       print "Invalid selection! Type 'z' to submit your code or enter a KEYPAD position (1, 2, 3, 4, Q, W, E, R, A, S, D, F): "
       respond
     end
-    submit if @response.downcase == 'z'
-    @response
+    submit if response == 'z'
+    response
   end
 
   def submit
@@ -310,44 +395,19 @@ class Human < Player
   end
 
   def key_is_valid?
-    return true if ['1','2','3','4','q','w','e','r','a','s','d','f','z'].include? @response.downcase
+    return true if ['1','2','3','4','q','w','e','r','a','s','d','f','z'].include? response
     false
   end
 
-  def make_a_code
-    respond
-    until code_is_valid?
-      puts "The code you have entered is invalid!"
-      puts "Code Maker, set a code of four pegs from these pegs (!, @, #, $, %, ^, & , *)."
-      print "Each peg must be different (e.g. !@#$): "
-      respond
-    end
-    @response.split('')
-  end
-
   def mode_is_valid?
-    if @response == '1' || @response == '2'
+    if response == '1' || response == '2'
       return true
     end
     false
   end
 
-  def play_round
-    loop do
-      print "Move from (KEYPAD position) or 'z' to submit code: "
-      chosen_peg = choose_key
-      break if chosen_peg.downcase == 'z'
-      print "Move to (KEYPAD position) or 'z' to submit code: "
-      peg_destination = choose_key
-      break if peg_destination.downcase == 'z'
-      @board.switch_pegs(chosen_peg, peg_destination)
-      @board.render
-    end
-    @board.assess_guess
-  end
-
   def quit?
-    @response == 'quit'
+    response == 'quit'
   end
 
   def respond
@@ -355,15 +415,6 @@ class Human < Player
     exit if quit?
   end
 
-  def set_mode
-    respond
-    until mode_is_valid?
-      puts "The mode you have entered is invalid!"
-      print "Press '1' or '2' and then enter, to set the mode: "
-      respond
-    end
-    @response
-  end
 end
 
 MasterMind.new.start_game
