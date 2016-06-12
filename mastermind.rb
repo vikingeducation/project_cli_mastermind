2d62fca7565f0db74c4f6fd1d665ba67c532cfe1
Deepak <grc.deepak@gmail.com>
2016-06-11 18:53:33 -0700

    rubocoped

--- mastermind.rb
+++ mastermind.rb
@@ -5,11 +5,11 @@
 # prev 2 steps loop for max 12 times
 # exit early of codebreaker guesses the secret
 #
-# 1. Player > CodeMaker, CodeBreaker
+# 1. Player > Human, Computer
 # 2. Board       - Array of Rows
 # 3. Rows        - pegs[4], :close , :exact
 # 3. Peg         - color
-#Verbs :
+# Verbs :
 # 1. place pegs on board - guess
 # 2. Compare code with secret
 # 3. Print number of close and exact pegs after each guess
@@ -22,15 +22,15 @@ class Peg
   end
 
   def ==(other_peg)
-    self.color == other_peg.color
+    color == other_peg.color
   end
 
   def to_s
-    puts "#{self.color}"
+    puts color.to_s
   end
 
   def eql?(other_peg)
-    self.color == other_peg.color
+    color == other_peg.color
   end
 
   def hash
@@ -38,14 +38,8 @@ class Peg
   end
 
   def color
-    case @color
-      when :r then "Red"
-      when :g then "Green"
-      when :b then "Blue"
-      when :y then "Yellow"
-      when :o then "Orange"
-      when :p then "Purple"
-    end
+    dict = { r: "Red", g: "Green", b: "Blue", y: "Yellow", o: "Orange", p: "Purple" }
+    dict[@color]
   end
 end
 
@@ -54,11 +48,12 @@ class Row
   def initialize(num_cols)
     @pegs = []
     num_cols.times { @pegs << nil }
-    @response = { close: 0, exact: 0}
+    @response = { close: 0, exact: 0 }
   end
 end
 
 class Player
+  PEG_ARR = ['r', 'g', 'b', 'p', 'y', 'o'].freeze
   def initialize(name)
     @name = name
   end
@@ -71,7 +66,7 @@ class Player
     true
   end
 
-  def get_input_from_player
+  def input_from_player
     input = []
     loop do
       puts "Enter Peg order>"
@@ -80,7 +75,7 @@ class Player
         exit
       else
         input = raw_input.split("")
-        break if validate_input(input, ['r', 'g', 'b', 'p', 'y', 'o'])
+        break if validate_input(input, PEG_ARR)
         puts "Please enter a valid input - 4 pegs"
         puts "Available pegs : Red(r) Green(g) Blue(b) Yellow(y) Orange(o) Purple(p)"
       end
@@ -92,8 +87,8 @@ class Player
     row = board.slots[index]
     response = row.response
     pegs = row.pegs
-    response[:exact]  = @secret.each_index.select { |i| @secret[i] == pegs[i] }.count
-    response[:close] =  @secret.each_index.select { |i| pegs.include?(@secret[i]) }.count - response[:exact]
+    response[:exact] = @secret.each_index.count { |i| @secret[i] == pegs[i] }
+    response[:close] = @secret.each_index.count { |i| pegs.include?(@secret[i]) } - response[:exact]
   end
 end
 
@@ -103,15 +98,15 @@ class Human < Player
     @secret = []
   end
 
-  def place_pegs(board, index,num_secret)
+  def place_pegs(board, index, _num_secret)
     row = board.slots[index]
-    get_input_from_player.each_with_index do |str, index|
-      row.pegs[index] = Peg.new(str.to_sym)
+    input_from_player.each_with_index do |str, i|
+      row.pegs[i] = Peg.new(str.to_sym)
     end
   end
 
-  def generate_secret(num_secret)
-    get_input_from_player.each_with_index do |str, index|
+  def generate_secret(_num_secret)
+    input_from_player.each do |str|
       @secret << Peg.new(str.to_sym)
     end
     @secret
@@ -126,7 +121,7 @@ class Computer < Player
 
   def generate_secret(num_secret)
     @secret = []
-    arr = ['r', 'g', 'b', 'p', 'y', 'o'].shuffle[0..num_secret-1].each do |str|
+    PEG_ARR.shuffle[0..num_secret - 1].each do |str|
       @secret << Peg.new(str.to_sym)
     end
     @secret
@@ -134,25 +129,24 @@ class Computer < Player
 
   def place_pegs(board, index, num_secret)
     row = board.slots[index]
-    (0..num_secret-1).each do |index|
-      row.pegs[index] = Peg.new(['r', 'g', 'b', 'p', 'y', 'o'].sample.to_sym)
+    (0..num_secret - 1).each do |i|
+      row.pegs[i] = Peg.new(PEG_ARR.sample.to_sym)
     end
   end
 end
 
 class Board
-  COLORS = %w(Red Green Blue Yellow Orange Purple)
   attr_accessor :slots
   def initialize(num_rows, num_cols)
     @slots = []
     num_rows.times { @slots << Row.new(num_cols) }
   end
 
-  def render(opts = {clear: true})
+  def render(opts = { clear: true })
     system "clear" if opts[:clear]
     @slots.each_with_index do |row, index|
       print index.to_s.ljust(3) + "|\t"
-      row.pegs.each {|peg| print peg ? peg.color+"\t\t" : "-"+"\t\t" }
+      row.pegs.each { |peg| print peg ? peg.color + "\t\t" : "-\t\t" }
       puts "| [close: #{row.response[:close]} exact: #{row.response[:exact]}]"
     end
   end
@@ -177,7 +171,7 @@ class Mastermind
     @board.render(clear: false)
     winner = nil
     puts
-    (0..@max_guess-1).each do |index|
+    (0..@max_guess - 1).each do |index|
       @code_breaker.place_pegs(@board, index, NUM_SECRET_PEGS)
       @code_maker.generate_response(@board, index)
       @board.render
@@ -219,15 +213,18 @@ class Mastermind
 
   def gameover_msg(winner)
     if winner
-      puts "Congratulations! You win" if @code_breaker.kind_of?(Human)
-      puts "Computer Wins! Your code is broken" if @code_breaker.kind_of?(Computer)
+      if @code_breaker.is_a?(Human)
+        puts "Congratulations! You win"
+      else
+        puts "Computer Wins! Your code is broken"
+      end
+    elsif @code_breaker.is_a?(Human)
+      puts "Sorry! You ran out of guesses"
     else
-      puts "Sorry! You ran out of guesses" if @code_breaker.kind_of?(Human)
-      puts "You win! Computer failed to break you awesome code" if @code_breaker.kind_of?(Computer)
+      puts "You win! Computer failed to break you awesome code"
       print "Secret Code is [\t"
-      @secret.each {|peg| print peg ? peg.color+"\t\t" : "-"+"\t\t" }
-      print " ]"
-      puts
+      @secret.each { |peg| print peg ? peg.color + "\t\t" : "-\t\t" }
+      print " ]\n"
     end
   end
 
@@ -237,9 +234,6 @@ class Mastermind
     response[:exact] == NUM_SECRET_PEGS
   end
 end
-
-
-
 
 game = Mastermind.new(12)
 game.play
