3dfac3327bba60470c405df075dae331bb1aa6c7
Conor Fischer <conor.fischer@gmail.com>
2016-06-28 00:05:05 -0500

    Add init tests

--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -6,8 +6,10 @@ describe Codebreaker do
 	end
 
 	it 'accepts a user name upon creation' do
+		expect{Codebreaker.new("Conor")}.not_to raise_error
 	end
 
 	it 'accepts no parameters upon creation' do
+		expect{Codebreaker.new()}.not_to raise_error
 	end
 end
