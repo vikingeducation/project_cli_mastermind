a864130f79d488c123103ae135e8b311b0ee88d3
Conor Fischer <conor.fischer@gmail.com>
2016-06-28 00:03:22 -0500

    Add init testst

---
+++ spec/codebreaker_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../lib/codebreaker.rb"
+
+describe Codebreaker do
+	it "is a Codemaker" do
+		expect(subject).to be_a(Codebreaker)
+	end
+
+	it 'accepts a user name upon creation' do
+	end
+
+	it 'accepts no parameters upon creation' do
+	end
+end
