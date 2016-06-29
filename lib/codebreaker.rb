f711b16944f729de3141eef157e88527c2777c95
Conor Fischer <conor.fischer@gmail.com>
2016-06-28 20:07:26 -0500

    Add basic functionality to codebreaker - render, make_guess, won?

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -3,10 +3,23 @@ require_relative './codecolor.rb'
 class Codebreaker
 	include Codecolor
 
-	attr_reader :name
+	attr_reader :name, :guesses_board
 
 	def initialize(name="Player 1")
 		@name = name
+		@guesses_board = []
 	end
 
+	def render
+		@guesses_board.reverse.each { |guess| print "|| #{guess[0]} , #{guess[1]} , #{guess[2]} , #{guess[3]} ||\n"}
+		print "||_______________||\n"
+	end
+
+	def make_guess(guess)
+		@guesses_board << guess
+	end
+
+	def won?(code)
+		@guesses_board[-1] == code ? true : false
+	end
 end
--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -12,4 +12,47 @@ describe Codebreaker do
 	it 'accepts no parameters upon creation' do
 		expect{Codebreaker.new()}.not_to raise_error
 	end
+
+	describe '#render' do
+		it "displays initial board" do
+			codebreaker = Codebreaker.new
+			expect{codebreaker.render}.to output("||_______________||\n").to_stdout
+		end
+
+		it "displays partial board" do
+			codebreaker = Codebreaker.new
+			codebreaker.make_guess(['r','r','r','r'])
+			codebreaker.make_guess(['b','b','b','y'])
+			expect{codebreaker.render}.to output("|| b , b , b , y ||\n|| r , r , r , r ||\n||_______________||\n").to_stdout
+		end
+	end
+
+	describe '#make_guess' do
+		it "adds 1 guess to guess board" do
+			codebreaker = Codebreaker.new
+			codebreaker.make_guess(['r','r','r','r'])
+			expect(codebreaker.guesses_board).to eq([['r','r','r','r']])
+		end
+
+		it "adds multiple guesses to guess board" do
+			codebreaker = Codebreaker.new
+			codebreaker.make_guess(['r','r','r','r'])
+			codebreaker.make_guess(['b','b','b','y'])
+			expect(codebreaker.guesses_board).to eq([['r','r','r','r'],['b','b','b','y']])
+		end
+	end
+
+	describe '#won' do
+		it 'returns true if game is won' do
+			codebreaker = Codebreaker.new
+			codebreaker.make_guess(['r','r','r','r'])
+			expect(codebreaker.won?(['r','r','r','r'])).to eq(true)
+		end
+
+		it 'returns false if game is not won' do
+			codebreaker = Codebreaker.new
+			codebreaker.make_guess(['r','r','r','r'])
+			expect(codebreaker.won?(['r','r','r','b'])).to eq(false)
+		end
+	end
 end
