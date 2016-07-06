1519a2af8f78122896569b5bdf6d2ced322a7faa
Morgan Martin <morganm5201@gmail.com>
2016-07-06 13:15:36 -0400

    Start mastermind pseudocode

---
+++ mastermind.rb
@@ -0,0 +1,28 @@
+#Game Class
+#Play method that runs everything
+#Turn method that handles turns
+#
+
+#Board Class
+
+#Player Class
+#Include CodeBreakerModule
+#Include CodeMakerModule
+
+#Human < Player
+
+#Computer < Player
+
+#Peg
+
+#CodePeg < Peg
+
+#KeyPeg < Peg
+
+
+* Game Class
+** Initialize
+*** Instantiate MasterCode thing
+*** Instantiage Players
+*** Instantiage Board
+*** Instantiage Pegs
