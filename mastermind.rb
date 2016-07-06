08aa98db993b0f9a46698f5c41c89dfe2d4903de
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-06 13:34:18 -0500

    initial commit

---
+++ mastermind.rb
@@ -0,0 +1,24 @@
+
+
+ game class
+  into
+  rules
+  number of player
+  gameloop
+
+
+player class
+  
+
+human class
+  inherit from player
+
+computer class
+  inherit from player
+
+ board class
+ render(drawing)
+ feedback algo
+
+ peg class
+  colors
