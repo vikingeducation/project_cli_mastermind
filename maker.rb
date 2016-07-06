f3d78b90cf2c848c71f733ac9d044d34bb0e344e
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-06 16:27:27 -0700

    feedback wrestling

--- maker.rb
+++ maker.rb
@@ -1,5 +1,8 @@
+require_relative 'player.rb'
+
 class Maker < Player
-  attr_reader :fb_board, :name, :code
+  attr_reader :fb_board, :name
+  attr_accessor :code
 
   def initialize
     @fb_board = Board.new
@@ -11,25 +14,73 @@ class Maker < Player
     @code = get_input
   end
 
-  def feedback(guess)
-    feedback_arr = []]
+  def feedback(guess, code)
+    feedback_arr = []
 
-    copy = guess
-    code_copy = @code
-    copy.each_with_index do |ele, i|
-      if code_copy[i] == copy[i]
-        feedback_arr.push("r")
-        copy[i] = "-"
-      end
+
+
+    # copy = guess
+    # code_copy = code
+    # copy.each_with_index do |ele, i|
+    #   if code_copy[i] == copy[i]
+    #     feedback_arr.push("r")
+    #     # copy.delete_at(i)
+    #     copy[i] = nil
+    #     code_copy[i] = "-"
+    #   end
+    # end
+    # copy.each_with_index do |ele, i|
+    #   if code_copy.include?(ele)
+    #     feedback_arr.push "w"
+    #     code_copy[code_copy.find_index(ele)] = "-"
+    #   end
+    # end
+    # feedback_arr
+  end
+
+  def exact_match_at(guess)
+    arr_exact = []
+    guess.each_with_index do |ele, i|
+      arr_exact << i if ele == code[i]
     end
-    copy.each_with_index do |ele, i|
-      if code_copy.include?(ele)
-        feedback_arr.push "w"
-        code_copy[code_copy.find_index(ele)] = "-"
-      end
-    end
+    arr_exact
+  end
+
+  def partial_match_at(guess)
+    arr_partial = []
+    color_hash = {}
+    guess.delete_at(exact_match_at(guess))
+    guess.sort
+    guess.each_with_index do |ele, i|
 
   end
 
+end
+
+def test
+
+  maker = Maker.new
+  code= %w[r r r r]
+  guess = %w[r r r r]
+  p maker.feedback(guess, code)
+
+  code= %w[b r w r]
+  guess = %w[b y r b]
+  p maker.feedback(guess, code)
+
+  code= %w[b r w r]
+  guess = %w[r b r w]
+  p maker.feedback(guess, code)
+
+  code= %w[b r w r]
+  guess = %w[b w r r]
+  p maker.feedback(guess, code)
+
+  code= %w[b b w y]
+  guess = %w[g r o p]
+  p maker.feedback(guess, code)
+
 
 end
+
+test
