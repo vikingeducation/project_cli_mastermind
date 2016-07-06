dbcabf7555e0e935b248f6d3509e2ec912afb40f
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-06 11:06:13 -0700

    Pseudocode classes

---
+++ mastermind.rb
@@ -0,0 +1,16 @@
+# Initialize board [Mastermind]
+  # getting difficulty, # of players, etc. [Mastermind]
+  # a player makes the code [Player]
+# Play loop [Mastermind]
+  # render board [board]
+  # allow player to guess or quit [Player]
+  # check for game over [Board]
+  # feedback [Board]
+
+Classes
+  Mastermind
+  board
+  player
+   human
+   computer
+  peg
