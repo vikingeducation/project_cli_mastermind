7e598212e2f3fb9c247ac727bfba2c4d38ed2aa7
David Watts <davidjeremywatts@gmail.com>
2016-07-06 15:10:14 -0700

    merge

--- breaker.rb
+++ breaker.rb
@@ -1,4 +1,6 @@
-class Breaker < Player
+require_relative 'player.rb'
+
+class Breaker #< Player
   attr_accessor :guess_board, :name
 
   def initialize
@@ -7,8 +9,15 @@ class Breaker < Player
   end
 
   def guess
-    get_code_input
-    make guess
-
+    input = get_code_input
+    @guess_board[@guess_board.find_index(Array.new(4))] = input
   end
+end
+
+def test
+  newboard = Array.new(2){Array.new(4)}
+  puts newboard
+  input = %w[r b g y]
+  newboard[newboard.find_index(Array.new(4))] = input
+  puts newboard
 end
