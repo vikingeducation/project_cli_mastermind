e48b7a03e5446bf125d197ed1e53bc938597d641
Alex Lach <alexglach@gmail.com>
2016-07-06 17:25:44 -0400

    starting to rubyify

--- mastermind.rb
+++ mastermind.rb
@@ -8,37 +8,54 @@ COLORS[peg]
 
 class Mastermind
 
-  #initialize
+  def initialize
     # assign codebreaker -> Player.create_player
+    @codebreaker = Player.create_player
     # assign codemaker -> Player.create_player
+    @codemaker = Player.create_player
     # create new board with setup from codebreaker
+    @board = Board.new(@codebreaker)
+  end
 
-  #play
+  def play
+
+    loop do 
     # render board [board]
+      @board.render
     # check for game over [Board]
-    # allow player to guess or quit [board]
+      break if @board.game_over?
+    # allow player to guess or quit and send to Board[Player]
+      @board.move(@codebreaker.get_move)
+    end
+
+  end
+
 
   # end game
 end
   
 class Board
   # COLORS
-  # TURNS
+  @game_turns = 12
 
-  # initialize
+  def initialize(player)
     # number of turns
+    @player_turns = 0
     # array of moves
-    # who the codebreaker and codemaker is
+    @moves = []
     # get number of pegs [ask player]
+    @pegs = player.get_pegs
     # get number of colors
+    @colors = player.get_colors
+  end
 
   # render
     # start as number_of_turns lines
     # array of pegs with color
     # go through each move so far and print that line, plus feedback for that line
 
-  # make move
-    # ask player for move
+  #  move
+    # add move to board
 
   # give feedback
     # look at current turn
@@ -61,7 +78,13 @@ class Player
   #initialize
     #name
 
-  #make_code
+  #get_move
+    #prompt for move
+    #input_code
+
+  #make_answer
+    #prompt for answer
+    #input_code
 
   # get board conditions
     #get pegs
@@ -76,24 +99,50 @@ end
    
 
 class Human < Player
-  #make_code
+  #input_code
     #ask for input
     #check for format
 
-  # get pegs
+  def get_pegs
+    
+    pegs = 0
+    until (4..6).include?(pegs)
+      puts "How many pegs do you want to play with? Choose 4-6."
+      pegs = gets.chomp.to_i
+    end
+    pegs
 
-  # get colors
+  end
+
+  def get_colors
+
+    colors = 0
+    until (2..8).include?(colors)
+      puts "How many colors do you want to play with? Choose 2-8."
+      colors = gets.chomp.to_i
+    end
+    colors
+
+  end
 
 end
    
 
 class Computer < Player
-  #make_code
+  #input_code
     #generate array of colors
 
-  # get pegs
+  def get_pegs
+    pegs = (4..6).sample
+    puts "You're playing with #{pegs} pegs."
+    pegs
+  end
 
-  # get colors
+  def get_colors
+    colors = (2..8).sample
+    puts "You're playing with #{colors} colors."
+    colors
+  end
 end
 
 
