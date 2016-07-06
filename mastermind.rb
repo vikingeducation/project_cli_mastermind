944faacd13c81a39e7929570d41ccbcdc2f82b4b
Vishal Lama <vishal.lama@gmail.com>
2016-07-06 16:06:57 -0600

    Write pseudocode for Mastermind

---
+++ mastermind.rb
@@ -0,0 +1,13 @@
+Set up the game initially
+  Create a game board
+  Create a couple of players
+    (codebreaker and codemaker)
+  Ask codemaker to set the win condition
+Run the game loop (maximum of 12 times).
+  Render the game board.
+  Ask codebreaker for their guess.
+  Respond accordingly.
+    If correct guess, declare winner, game over. Stop looping.
+    Else update board.
+Game over. No winner.
+  
