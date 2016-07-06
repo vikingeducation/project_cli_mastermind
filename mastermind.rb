e5dfe0eaa77f99a74c7d4fd19040baf6f06323b2
cjvirtucio87 <cjv287@gmail.com>
2016-07-06 10:40:35 -0700

    Added classes.

--- mastermind.rb
+++ mastermind.rb
@@ -1,44 +1,57 @@
-BOARD
-|O O O O|[o,o,o,o]
-|O O O O|[o,o,o,o]
-|O O O O|[o,o,o,o]
-|O O O O|[o,o,o,o]
-|O O O O|[o,o,o,o]
+class Board
+
+  
+
+end
 
 
-CODE PEGS (for large holes)
-1. red
-2. green
-3. blue
-4. violet
-5. orange
-6. yellow
+class Player
 
-KEY PEGS (for small holes)
-1. black
-2. white
+end
 
-Player
-1. Codemaker < Player
-2. Codebreaker < Player
+class Codemaker < Player
 
-Codemaker
-- chooses a pattern of four pegs
-- duplicates are allowed
-- place pattern on a row (only visible to Codemaker)
+  def make_pattern
+  end
 
-Codebreaker
-- tries to guess pattern
-- a. order
-- b. color
-- only has 12 turns
-- how?
-- a. make a pattern
-- b. place pattern on row
+  def feedback
+  end
 
-Win State
-- Codemaker gives feedback using KEY PEGS
-- a. can provide 0 to 4 key pegs
-- b. black key peg => CORRECT in color and position
-- c. white key peg => CORRECT in color, but not position
+  def check_guess(guess)
+  end
 
+end
+
+
+class Codebreaker < Player
+
+  def get_guess
+
+  end
+
+end
+
+class Game
+
+  def render
+
+  end
+
+end
+
+
+class Peg
+
+
+
+end
+
+
+class CodePeg < Peg
+
+
+end
+
+class KeyPeg < Peg
+
+end
