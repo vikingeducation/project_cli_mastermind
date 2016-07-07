ef6eab3776a26de8c3bf50288f07c4b4ae656ccf
Hannah Squier <hannahsquier@gmail.com>
2016-07-06 17:47:53 -0700

    modify computer to not include smart AI

--- computer.rb
+++ computer.rb
@@ -2,19 +2,19 @@ require_relative 'player'
 class Computer < Player
 
   def get_code
-    # 4.times do
-    #   @guess << LEGAL_COLORS.sample
-    # end
-    # @guess
+    4.times do
+      @guess << LEGAL_COLORS.sample
+    end
+    @guess
   end
 
-  def get_colors
-    LEGAL_COLORS.each do |color|
-      @guess = %w(color color color color)
-      @board.add_guess(@breaker.guess)
-      @board.get_feedback(@breaker.guess, @secret_code)
-    end
-  end
+  # def get_colors
+  #   LEGAL_COLORS.each do |color|
+  #     @guess = %w(color color color color)
+  #     @board.add_guess(@breaker.guess)
+  #     @board.get_feedback(@breaker.guess, @secret_code)
+  #   end
+  # end
 
   def get_secret_code
     response = []
