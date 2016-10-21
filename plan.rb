9796747fcb7d0152a013749a68322312a16057a8
ttscott2 <timothytscott3@gmail.com>
2016-10-20 21:47:52 -0500

    add pseudocode

---
+++ plan.rb
@@ -0,0 +1,14 @@
+# Set up the game initially [Mastermind]
+  # Create a game board (empty row of 4 holes) [Board]
+  # Create a couple players [Player]
+# Start the game loop [Mastermind]
+  # Render the board [Board]
+  # Ask for and validate the current player's guess [Player]
+  # If the game should end [Mastermind]
+    # Display the proper success/failure message
+    # Stop looping
+
+# Objects required: board, code pegs (6 different colors [Red, Orange, Blue, Green, Yellow, Purple]),
+# cont. key pegs (black/white)
+
+#
