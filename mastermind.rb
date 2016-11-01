a4d38498b54eca94ac7e75825c8961c3c0ce0e66
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-01 14:20:44 -0700

    Luke drive

--- mastermind.rb
+++ mastermind.rb
@@ -1,4 +1,40 @@
 class Mastermind
+
+  def initialize()
+    @board = Board.new
+    @codemaker = Codemaker.new
+    @codebreaker = Codebreaker.new
+  end
+
+  def play
+    instructions
+    loop do
+      take_turn
+    end
+  end
+
+  def take_turn
+    #
+  end
+
+  def instructions
+    #
+  end
+
+  def gameover?
+    #
+  end
+
+  def win?
+    #
+  end
+
+  def lose?
+    #
+  end
+end
+
+class Board
 end
 
 class Codebreaker
@@ -7,8 +43,34 @@ end
 class Codemaker
 end
 
+Mastermind.new.play
 
-class CodePeg
-  # colors will be green, blue, yellow, red, black, white
-end
+#Board
+  #render
+  #full?
+  #number_of_right_answers
+  #number_of_right_colors: green, blue, yellow, red, black, white
 
+#Codebreaker
+  # enter_pegs
+  #valid_input?
+  #right number of pegs
+  # correct colors?
+
+
+#Codemaker
+  #set up board
+
+#Mastermind
+  #play
+  #gameover?
+  #win?
+  #lose?
+
+
+#
+#Mastermind
+  #play
+  #gameover?
+  #win?
+  #lose?
