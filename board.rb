f0b02c53d58c8cc12fa2a67de2ee5d6a683d37b9
Christian Carey <christianrcarey@gmail.com>
2016-11-01 14:09:59 -0400

    commit

--- board.rb
+++ board.rb
@@ -1,11 +1,19 @@
 class Board 
+  MAX_TURNS = 12 
   attr_writer :code
 
   def initialize
     @code = nil
+    @board_guesses = []
+    @turn_count = 0
   end
 
   def render
+  end
+
+  def add_choice(choice)
+    @board_guesses << choice
+    @turn_count += 1
   end
 
 end
