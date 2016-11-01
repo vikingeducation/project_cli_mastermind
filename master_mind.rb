274ca2e698c1aa828c4e75b610f325cc1d802bb5
Jessica G <gillan.jessica@gmail.com>
2016-11-01 11:19:10 -0600

    define game classes

--- master_mind.rb
+++ master_mind.rb
@@ -1,6 +1,7 @@
 # set up game and board (human as codebreaker) -- mastermind makes a board and players
-# set up code -- player
+
 # start game -- mastermind
+# set up code -- player
 # each turn: -- mastermind
   # render board -- board
   # get player guess -- player
@@ -11,3 +12,66 @@
     # new turn if game not over -- mastermind
 # output results -- mastermind??
 # play again? -- mastermind
+ 
+class Mastermind
+	CHOICES = %w(A B C D E F)
+
+	def initialize
+		@board = Board.new
+		@code_maker = ComputerPlayer.new
+		@code_breaker = HumanPlayer.new
+	end
+
+	def play
+		@board.code = @code_maker.make_code
+		@board.render
+		@code_breaker.get_choice
+		break if game_over?
+
+	end 
+end
+
+class Player
+end
+
+class ComputerPlayer < Player
+	def make_code
+		code = []
+		4.times do 
+			code << Mastermind::CHOICES.sample
+		end
+		code
+	end
+end
+
+class HumanPlayer < Player
+
+	def get_choice
+		puts "Try to guess the four letter code from A,B,C,D,E,F: (ie ABCD)"
+		puts ">"
+		gets.strip.chars
+	end
+end
+
+class Board 
+	attr_writer :code
+
+	def initialize
+		@code = nil
+	end
+
+	def render
+	end
+
+end
+
+
+
+
+
+
+
+
+
+
+
