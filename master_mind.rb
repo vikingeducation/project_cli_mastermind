338fed205ff2ad0931746ddca1d0310cc8c484b9
Christian Carey <christianrcarey@gmail.com>
2016-11-01 12:55:57 -0400

    start mastermind

---
+++ master_mind.rb
@@ -0,0 +1,13 @@
+# set up game and board (human as codebreaker) -- mastermind makes a board and players
+# set up code -- player
+# start game -- mastermind
+# each turn: -- mastermind
+  # render board -- board
+  # get player guess -- player
+  # give feedback -- board
+  # check for game over -- board
+    # check for victory -- board
+    # check for loss -- board
+    # new turn if game not over -- mastermind
+# output results -- mastermind??
+# play again? -- mastermind
