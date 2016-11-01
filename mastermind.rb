6c9fb08eee5177e9be0d7510dd4ffeb29ac45c1f
Alexa <alexa.anderson@outlook.com>
2016-11-01 13:29:31 -0500

    Pseudocode

---
+++ mastermind.rb
@@ -0,0 +1,31 @@
+# There's a
+#   - game
+#     - 12 rounds
+#     - answer
+#     - board
+#       - rows
+#       - pegs
+
+# Class Game
+# - turn
+# - win conditions
+
+# Class Board
+# - render (module?)
+# - builds rows
+
+# Class Row
+# - compare (module?)
+# * Class SecretCode (built by codemaker)
+# * Class GuessRow (built by breaker)
+# * Class BoardRow (what gets displayed, held in board)
+
+# Class Peg
+# - color
+# - position
+
+# Class CodeBreaker
+# - guess
+
+# Class CodeMaker
+# - makes code
