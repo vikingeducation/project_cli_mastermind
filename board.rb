f7a0404ac735d5869ef990f31b2e247af1458e1b
Robert Wayne <robertjwayne@gmail.com>
2017-02-07 12:54:53 -0500

    Add additional methods to outline of Board class

--- board.rb
+++ board.rb
@@ -8,9 +8,25 @@ class Board
       # display an existing peg if any, else blank
 
   # add_guess
+    # IF valid_guess?
+      # place guess
+    # ELSE
+      # display error message
+
+  # valid_guess?
+    # is each peg in the guess a valid_color?
+
+  # valid_color?
+    # is the peg included in the list valid_colors?
+
+  # close_pegs
+    # number of pegs in the guess with correct color, but incorrect location
+
+  # exact_pegs
+    # number of pegs in the guess with correct color, and correct location
 
   # winning_combination?
-    # return if the guess is equal to the secret code
+    # does the guess have 4 exact_pegs?
 
   # full?
     # does every row contain a guess?
