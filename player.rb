052437a5441591515c86e6c8c1d2fb54dde4fa3f
Robert Wayne <robertjwayne@gmail.com>
2017-02-07 13:17:17 -0500

    Add set_code method to Player class

--- player.rb
+++ player.rb
@@ -2,11 +2,18 @@
 class Player
   # initialize
 
+  # set_code
+    # loop infinitely
+      # ask_for_code
+      # IF validate_code_format is true
+        # IF code can be set on the board
+          # break the loop
+
   # get_code
     # loop infinitely
       # ask_for_code
       # IF validate_code_format is true
-        # IF piece can be placed on board
+        # IF guess can be placed on board
           # break the loop
 
   # ask_for_code
