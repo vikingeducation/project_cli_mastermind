eea2f7405309b387aa0499532e9e78f8b69c07ee
Robert Wayne <robertjwayne@gmail.com>
2017-02-07 12:07:56 -0500

    Outline of Mastermind class

---
+++ mastermind.rb
@@ -0,0 +1,27 @@
+# Controls the game play
+class Mastermind
+  # initialize
+    # set up the board
+    # set up the players
+    # assign the codemaker player
+    # assign the codebreaker player
+
+  # play
+    # ask for codebreaker to set code
+    # loop infinitely
+      # call the board rendering method
+      # ask for guess from codebreaker player
+      # break the loop if the game is over
+
+  # check_game_over
+    # check_victory
+    # check_loss
+
+  # check_victory
+    # IF board says codebreaker's guess is a winning_combination?
+      # display a victory message
+
+  # check_loss
+    # IF board says the board is full?
+      # display a loss message
+end
