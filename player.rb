d27e2a2dc9329e5310efbdd4697379dada981c1b
Robert Wayne <robertjwayne@gmail.com>
2017-02-07 13:48:29 -0500

    Add initialize method to Player class

--- player.rb
+++ player.rb
@@ -1,6 +1,12 @@
 # Manages all player related functionality
 class Player
+  attr_accessor :name
+
   # initialize
+  def initialize(name = "Mystery Player", board)
+    @name = name
+    @board = board
+  end
 
   # set_code
     # loop infinitely
@@ -9,14 +15,18 @@ class Player
         # IF code can be set on the board
           # break the loop
 
-  # get_code
+  # ask_for_code
+    # display message asking for code
+    # pull code from command line
+
+  # guess_code
     # loop infinitely
-      # ask_for_code
+      # ask_for_guess
       # IF validate_code_format is true
         # IF guess can be placed on board
           # break the loop
 
-  # ask_for_code
+  # ask_for_guess
     # display message asking for code
     # pull code from command line
 
