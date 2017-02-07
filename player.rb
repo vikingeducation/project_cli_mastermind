326a82709ff7e25442d37345a2fe5b1f0499b5fb
Robert Wayne <robertjwayne@gmail.com>
2017-02-07 13:53:19 -0500

    Add set_code method to Player class

--- player.rb
+++ player.rb
@@ -9,11 +9,22 @@ class Player
   end
 
   # set_code
+  def set_code
     # loop infinitely
+    loop do
       # ask_for_code
+      ask_for_code
+
       # IF validate_code_format is true
+      if validate_code_format
         # IF code can be set on the board
+        if @board.set_code
           # break the loop
+          break
+        end
+      end
+    end
+  end
 
   # ask_for_code
     # display message asking for code
