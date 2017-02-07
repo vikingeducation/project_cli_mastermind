604a0950df8e37c50cec57d90ff703d2a47d295b
Robert Wayne <robertjwayne@gmail.com>
2017-02-07 17:06:53 -0500

    Add ComputerPlayer class

---
+++ computer_player.rb
@@ -0,0 +1,17 @@
+class ComputerPlayer < Player
+  def initialize
+    super
+    @name = "Computer"
+  end
+
+  # ask for code to set secret code
+  def ask_for_code
+
+  end
+
+  # ask_for_guess
+  def ask_for_guess
+
+  end
+
+end
