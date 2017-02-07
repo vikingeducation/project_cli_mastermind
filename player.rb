4c9cfb9ca7038d91a8987aa14b558c75a20ec399
Robert Wayne <robertjwayne@gmail.com>
2017-02-07 12:15:13 -0500

    Outline of Player class

---
+++ player.rb
@@ -0,0 +1,20 @@
+# Manages all player related functionality
+class Player
+  # initialize
+    # Set role (codebreaker or codemaker)
+
+  # get_code
+    # loop infinitely
+      # ask_for_code
+      # IF validate_code_format is true
+        # IF piece can be placed on board
+          # break the loop
+
+  # ask_for_code
+    # display message asking for code
+    # pull code from command line
+
+  # validate_code_format
+    # UNLESS code is in the proper format
+      # display error message
+end
