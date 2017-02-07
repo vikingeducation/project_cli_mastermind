0d65d82a60f3a6cf1543b084fd4fa115ed420b9f
Robert Wayne <robertjwayne@gmail.com>
2017-02-07 13:20:20 -0500

    Add set_code method to Board class

--- board.rb
+++ board.rb
@@ -7,17 +7,23 @@ class Board
     # loop through data structure
       # display an existing peg if any, else blank
 
+  # set_code
+    # IF valid_code?
+      # set code
+    # ELSE
+      # display error message
+
   # add_guess
-    # IF valid_guess?
+    # IF valid_code?
       # place guess
     # ELSE
       # display error message
 
-  # valid_guess?
-    # is each peg in the guess a valid_color?
+  # valid_code?
+    # is each peg in the code a valid_color?
 
   # valid_color?
-    # is the peg included in the list valid_colors?
+    # is the peg included in the list of valid_colors?
 
   # close_pegs
     # number of pegs in the guess with correct color, but incorrect location
