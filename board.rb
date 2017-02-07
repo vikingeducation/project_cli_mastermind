3145f2b20999e0259b8326350635ae2beebcd596
Robert Wayne <robertjwayne@gmail.com>
2017-02-07 12:25:33 -0500

    Outline of Board class

---
+++ board.rb
@@ -0,0 +1,17 @@
+# Maintains game board state
+class Board
+  # initialize board
+    # set up blank data structure
+
+  # render
+    # loop through data structure
+      # display an existing peg if any, else blank
+
+  # add_guess
+
+  # winning_combination?
+    # return if the guess is equal to the secret code
+
+  # full?
+    # does every row contain a guess?
+end
