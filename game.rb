1021e61190a6beda86c5c263d23496bd8d1ade14
mac718 <mac718@gmail.com>
2017-09-24 10:48:53 -0400

    wrapped all instance variable in Game in methods

--- game.rb
+++ game.rb
@@ -3,6 +3,8 @@ require_relative 'code_breaker'
 require_relative 'board'
 
 class Game
+  attr_accessor :codemaker, :codebreaker, :board, :code, :guess
+  
   def initialize
     @codemaker = CodeMaker.new
     @codebreaker = CodeBreaker.new
@@ -10,23 +12,27 @@ class Game
     @code = @codemaker.code
   end
 
+  def assign_guess
+    @guess = codebreaker.make_guess
+  end
+
   def guess_sequence
-    @board.prompt_for_guess
-    @guess = @codebreaker.make_guess
-    @board.add_guess(@guess)
-    @board.increment_guess_number
+    board.prompt_for_guess
+    assign_guess
+    board.add_guess(guess)
+    board.increment_guess_number
   end
 
   def update_board
-    @board.create_hint(@guess, @code)
-    @board.display_board
+    board.create_hint(guess, code)
+    board.display_board
   end
 
   def play
-    while @guess != @code && @board.guess_number <= 12
+    while guess != code && board.guess_number <= 12
       guess_sequence
       update_board
     end
-    @guess == @code ? (puts 'Good Job! You win!') : (puts 'Sorry, no more guesses!')
+    guess == code ? (puts 'Good Job! You win!') : (puts 'Sorry, no more guesses!')
   end
 end
